e8a0bdca49816c6976261da678368389f977ac26
simon <icelafone@gmail.com>
2025-07-10 23:47:44 +0800

    建立 JWT 驗證 Middleware（保護 API）

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"SP2/controllers"
+	"SP2/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +13,12 @@ func SetupRoutes(r *gin.Engine) {
 	})
 	r.POST("/register", controllers.Register)
 	r.POST("/login", controllers.Login)
+	// Protected group
+	auth := r.Group("/")
+	auth.Use(middlewares.AuthMiddleware())
 
+	// 之後會放：商品、訂單等路由
+	auth.GET("/protected", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Token passed. You are authorized!"})
+	})
 }
